575e44b6b4974a832d00a87e920822081635a0a2
Jonathan <jrfreder@gmail.com>
2018-05-29 14:03:00 -0400

    testing for home_page view called by /

--- rpg_toolkit/characters/tests.py
+++ rpg_toolkit/characters/tests.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
 
 class SmokeTest(TestCase):
-    def test_for_smoke(self):
-        self.assertEqual(2, 3)
+    def test_math(self):
+        self.assertEqual(1+1, 2)
+    def test_truthiness(self):
+        self.assertTrue(True)
+
+
+from django.urls import resolve
+from characters.views import home_page
+
+class HomePageTest(TestCase):
+    def test_root_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
