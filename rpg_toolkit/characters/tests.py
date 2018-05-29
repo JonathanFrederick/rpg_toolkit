a4bf30c6f098e81985ac131bce9fd3599eba20a5
Jonathan <jrfreder@gmail.com>
2018-05-29 15:09:30 -0400

    testing view returns basic html

--- rpg_toolkit/characters/tests.py
+++ rpg_toolkit/characters/tests.py
@@ -8,9 +8,18 @@ class SmokeTest(TestCase):
 
 
 from django.urls import resolve
+from django.http import HttpRequest
 from characters.views import home_page
+
 
 class HomePageTest(TestCase):
     def test_root_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Character Creation</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
