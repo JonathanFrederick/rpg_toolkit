8fd359154793a5c50547b7d25fa69c660ddc586a
Jonathan <jrfreder@gmail.com>
2018-05-30 16:34:02 -0400

    shuffling and testing for total span

--- functional_tests.py
+++ functional_tests.py
@@ -8,7 +8,7 @@ class TestAbilityScoreFunctionality:
     def teardown_method(self):
         self.browser.close()
 
-    def test_character_creation_title(self):
+    def test_character_ability_score_view(self):
         # check title and header for accurate text
         assert 'Character Creation' in self.browser.title
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -21,12 +21,14 @@ class TestAbilityScoreFunctionality:
         assert top_box.tag_name == 'input'
         assert top_box.get_attribute('value') == '10'
 
-        # check that for arrows that increase and decrease values
-
         # check that total points spent is accurately calculated
+        total_points = self.browser.find_element_by_class_name('total-points')
+        assert total_points.get_attribute('innerHTML') == '0'
 
         # check for input boxes for racial bonus with 0 for the initial value
 
         # check that racial bonuses are accurately applied to ability totals
 
         # check for accurate modifier calculations
+
+        # check for arrows that increase and decrease values
