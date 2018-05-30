ff055dded0432730a270e68b4d34f76beda68714
Jonathan <jrfreder@gmail.com>
2018-05-29 23:08:31 -0400

    just checking the right template is rendered

--- rpg_toolkit/characters/tests.py
+++ rpg_toolkit/characters/tests.py
@@ -10,7 +10,7 @@ class SmokeTest(TestCase):
 from django.urls import resolve
 from django.http import HttpRequest
 from characters.views import home_page
-
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     def test_root_resolves_to_home_page_view(self):
@@ -20,6 +20,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>Character Creation</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
