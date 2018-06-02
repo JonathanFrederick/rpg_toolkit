22cdd309f71c72c963adc3a6427b4d0b31990c10
Jonathan <jrfreder@gmail.com>
2018-06-02 16:53:38 -0400

    using jinja for loop with list of abilities

--- rpg_toolkit/characters/views.py
+++ rpg_toolkit/characters/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home_page(request):
-    return render(request, 'home.html')
+    context = {'abilities': ["STR", "DEX", "CON", "INT", "WIS", "CHA"]}
+    return render(request, 'home.html', context=context)
 
 def test_js(request):
     return render(request, 'tests.html')
