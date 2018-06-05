0b71d9b1b93cfd5e89f8ae5fe511966e1990511f
Jonathan <jrfreder@gmail.com>
2018-06-05 00:53:37 -0400

    scrubbing out weird minuses

--- rpg_toolkit/characters/management/commands/populate.py
+++ rpg_toolkit/characters/management/commands/populate.py
@@ -23,7 +23,7 @@ race_list_urls = [
 def get_races():
     for list_url in race_list_urls:
         page = BS(requests.get(list_url).text, 'html.parser')
-        for r in page.find('div',class_='body').find_all('a',href=only_main)[-3:-2]:
+        for r in page.find('div',class_='body').find_all('a',href=only_main)[:-2]:
             new_race = Race()
             race_page = BS(requests.get('http://paizo.com'+r.get('href')).text,
                 'html.parser')
@@ -42,5 +42,5 @@ def get_races():
                         #         new_race.race = re.sub(r'-racial-traits', r'', trait.id)
                         #         print(new_race.race)
                     if trait.b and '+' in trait.b.text:
-                        new_race.ability_bonus = trait.b.text
+                        new_race.ability_bonus = re.sub(r'–', r'-', trait.b.text)
             new_race.save()
