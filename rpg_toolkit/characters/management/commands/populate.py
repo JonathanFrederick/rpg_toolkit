c45e5287ca92bed75aaa6171eb823a525b5137dd
Jonathan <jrfreder@gmail.com>
2018-06-16 15:35:02 -0400

    not creating duplicate races

--- rpg_toolkit/characters/management/commands/populate.py
+++ rpg_toolkit/characters/management/commands/populate.py
@@ -26,10 +26,14 @@ def get_races():
     for list_url in race_list_urls:
         page = BS(requests.get(list_url).text, 'html.parser')
         for r in page.find('div',class_='body').find_all('a',href=only_main):
-            new_race = Race()
-            new_race.prd_url = 'http://paizo.com'+r.get('href')
+            prd_url = 'http://paizo.com'+r.get('href')
+            try:
+                curr_race = Race.objects.get(prd_url=prd_url)
+            except Race.DoesNotExist:
+                curr_race = Race()
+                curr_race.prd_url = prd_url
 
-            race_page = BS(requests.get(new_race.prd_url).text,
+            race_page = BS(requests.get(curr_race.prd_url).text,
                 'html.parser')
             traits = race_page.find('div',class_='body').contents
             for trait in list(traits):
@@ -38,13 +42,13 @@ def get_races():
                         current_section = trait.get('id')
                         if 'racial-traits' in trait['id'] and not \
                             'alternate' in trait['id']:
-                            new_race.race = re.sub(r' Racial Traits', r'', trait.text)
+                            curr_race.race = re.sub(r' Racial Traits', r'', trait.text)
 
                         # if trait.id:
                         #     print(trait.id)
                         #     if 'racial-traits' in trait.id and not 'alternate' in trait.id:
-                        #         new_race.race = re.sub(r'-racial-traits', r'', trait.id)
-                        #         print(new_race.race)
+                        #         curr_race.race = re.sub(r'-racial-traits', r'', trait.id)
+                        #         print(curr_race.race)
                     if trait.b and '+' in trait.b.text:
-                        new_race.ability_bonus = re.sub(r'–', r'-', trait.b.text)
-            new_race.save()
+                        curr_race.ability_bonus = re.sub(r'–', r'-', trait.b.text)
+            curr_race.save()
